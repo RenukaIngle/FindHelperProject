dda316a0ffc6d1c0ebfcc7e7dba8c3c0cf6e9d12
RenukaIngle <ingle.renu95@gmail.com>
2024-10-09 20:20:52 +0530

    Project file addeds

---
+++ Backend/src/main/java/com/app/entity/ImageEntity.java
@@ -0,0 +1,46 @@
+package com.app.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class ImageEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long imageId;
+    
+    @Lob
+    private byte[] data;
+    
+    private String type;
+    private String name;
+	
+ // Getters and setters
+	public Long getImageId() {
+		return imageId;
+	}
+	public void setImageId(Long imageId) {
+		this.imageId = imageId;
+	}
+	public byte[] getData() {
+		return data;
+	}
+	public void setData(byte[] data) {
+		this.data = data;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+    
+}
+
